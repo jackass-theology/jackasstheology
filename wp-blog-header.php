d4cf6f5b64288edc851697218dad2b91be1ce737
Ryan MacDiarmid <ryanmacdiarmid@gmail.com>
2019-03-15 12:16:47 -0700

    Mail Chimp Script in Header
    Pasted Mail Chimp Script in Header

--- wp-blog-header.php
+++ wp-blog-header.php
@@ -17,5 +17,7 @@ if ( ! isset( $wp_did_header ) ) {
 
 	// Load the theme template.
 	require_once( ABSPATH . WPINC . '/template-loader.php' );
+    
+    <script id="mcjs">!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/c5c049f1d3ee884a3235fc7d2/3b94da1d4a02050ca2ba595fe.js");</script>
 
 }
